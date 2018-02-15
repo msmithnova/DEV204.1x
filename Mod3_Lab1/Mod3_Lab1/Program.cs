ea8104f38fbea51117f97d108bb9aa1649c0f653
Michael Smith <mikesmithnova@gmail.com>
2018-02-15 04:25:08 -0400

    Module 3 Lab 1 complete

---
+++ Mod3_Lab1/Mod3_Lab1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum1(20, 40);
+
+            int result = Sum(20, 40);
+            Console.WriteLine($"The sum of 20 and 40 is: {result}");
+
+            int result2 = Sum(20, 40);
+            Console.WriteLine($"Calling Sum() with two arguments, result is: {result2}");
+
+            int result3 = Sum(10, 50, 80);
+            Console.WriteLine($"Calling Sum() with three arguments, result is: {result3}");
+
+            double dblResult = Sum(20.5, 30.6);
+            Console.WriteLine($"Calling Sum() that takes doubles result in: {dblResult}");
+        }
+
+        // Sum1() method that takes two integer arguments and sums them
+        // The method returns no value which is why we use void
+        // We also need to use static in the method signature because Main is static
+        // and you cannot call a non-static method from a static method
+        static void Sum1(int first, int second)
+        {
+            int sum = first + second;
+            Console.WriteLine($"The sum of {first} and {second} is: {sum}");
+        }
+
+        // Sum() method that takes two integer arguments and sums them
+        // The method returns an integer value which is why we use int in the signature
+        // We also need to use static in the method signature because Main is static
+        // and you cannot call a non-static method from a static method
+        static int Sum(int first, int second)
+        {
+            int sum = first + second;
+            return sum;
+        }
+
+        // Sum() method that takes three integer arguments and sums them
+        // and then returns the value
+        // This method uses the same name as the Sum() method that takes two integers
+        // but the parameters here indicate the method is expecting three integers as arguments
+        // The compiler knows which method to call based on the number of arguments passed in
+        static int Sum(int first, int second, int third)
+        {
+            int sum = first + second + third;
+            return sum;
+        }
+
+        // Sum() method that takes two doubles as arguments
+        // This method uses the same name as the Sum() method that takes two integers
+        // but the parameters here indicate the method is expecting two doubles as arguments
+        // The compiler knows which method to call based on the arguments data types
+        static double Sum(double first, double second)
+        {
+            double result = first + second;
+            return result;
+        }
+    }
+}
