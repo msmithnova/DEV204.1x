82996cbc4faa20ecdf0e86dfe82e247ce0468b1e
Michael Smith <mikesmithnova@gmail.com>
2018-02-15 03:54:11 -0400

    Module 2 Lab 2 complete

---
+++ Mod2_Lab2/Mod2_Lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a switch block
+
+            Console.WriteLine("Coffee sizes: 1=small 2=medium 3=large");
+            Console.Write("Please enter your selection: ");
+            string str = Console.ReadLine();
+            int cost = 0;
+
+            switch (str)
+            {
+                case "1":
+                case "small":
+                    cost += 25;
+                    break;
+                case "2":
+                case "medium":
+                    cost += 50;
+                    break;
+                case "3":
+                case "large":
+                    cost += 75;
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. Please select 1, 2, or 3.");
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Please insert {0} cents.", cost);
+            }
+            Console.WriteLine("Thank you for your business.");
+        }
+    }
+}
