0cd7428add94d6f96e850be99fdc29068c554de9
Michael Smith <mikesmithnova@gmail.com>
2018-02-15 03:10:56 -0400

    Module 1 Lab 1 complete

---
+++ Mod1_Lab1/Mod1_Lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create variables of different data types
+            // initialize with a "default" value
+            string firstName = "";
+            string lastName = "";
+            int age = 0;
+            string street = "";
+            string city = "";
+            string country = "";
+            DateTime birthDate;
+
+            // Assign some values
+            firstName = "Tom";
+            lastName = "Thumb";
+            age = 18;
+            street = "123 Fourth Street";
+            city = "Anytown";
+            country = "MyCountry";
+            birthDate = new DateTime(1932, 6, 1);
+
+            // output to the console window
+
+            // use simple output with just variable name
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            // use placeholder style
+            Console.WriteLine("{0} years old.", age);
+
+            // use string concatenation
+            Console.WriteLine(street + ", " + city + ", " + country);
+
+            // use string interpolation
+            // NOTE: This line of code only works with Visual Studio 2015 or C# 6.0 and later.
+            // If you are using an earlier version, then comment out this line of code.
+            Console.WriteLine($"Born on {birthDate}");
+        }
+    }
+}
