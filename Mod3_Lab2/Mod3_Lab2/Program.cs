8b943b07b50d3cd1ebd09c18edc07b9d57cf1365
Michael Smith <mikesmithnova@gmail.com>
2018-02-15 04:29:11 -0400

    Module 3 Lab 2 complete

---
+++ Mod3_Lab2/Mod3_Lab2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 9;
+            int second = 0;
+            Console.WriteLine("Enter your first number: ");
+            first = System.Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number: ");
+            second = System.Int32.Parse(Console.ReadLine());
+
+            int result = Divide(first, second);
+            Console.WriteLine($"The result of dividing {first} by {second} is {result}");
+        }
+
+        // Method Divide() accepts two integer arguments and returns the result
+        // of dividing first by second
+        // We do no validation of the the arguments so a user may pass in a zero
+        // in place of the second parameter.   This is an illegal operation in math
+        // If we don't validate input or catch the error, the application will crash
+        static int Divide(int first, int second)
+        {
+            int result = 0;
+            try
+            {
+                result = first / second;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero, please provide a non-zero value for your second value");
+            }
+
+            return result;
+        }
+    }
+}
