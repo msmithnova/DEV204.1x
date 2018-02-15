746a877b07436a7bfb5befd8928897159a3a1765
Michael Smith <mikesmithnova@gmail.com>
2018-02-15 04:02:14 -0400

    Module 2 Lab 4 complete

---
+++ Mod2_Lab4/Mod2_Lab4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a while loop
+            // We start with n = 1
+            // The condition check for the while, tests if n is less than 6, if so, the loop body code executes
+            // inside the loop, we output the value of n and then increment it by 1
+            // Once n = 6, the loop stops
+            // Pay attention to the output to see what the last value is to ensure you understand
+            // how the evaluation of the condition is done and how the while loop executes
+            int n = 1;
+            while (n < 6)
+            {
+                Console.WriteLine($"Current value of n is {n}");
+                n++;
+            }
+
+            // Create a do loop, also known as a do..while loop
+            // Note that with the do loop, the loop will run at least once regardless of the value of x
+            // which is due to the condition not being checked until the end.
+            // Experiment with this by setting x to a value greater than 5 and run the code
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 5);
+        }
+    }
+}
---
+++ Mod2_Lab4/Mod2_Lab4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mod2_Lab4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mod2_Lab4")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("67a070eb-121e-4d74-a305-1a43e5a66690")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
