aa676a3c2e01477ce5c3b1c9a5b9e510862ebb30
Michael Smith <mikesmithnova@gmail.com>
2018-02-15 04:15:34 -0400

    Module 2 Pattern Lab complete

---
+++ Mod2_Pattern/Mod2_Pattern/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                int j = 0;
+                while (j < 8)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write('X');
+                        }
+                        else
+                        {
+                            Console.Write('O');
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write('O');
+                        }
+                        else
+                        {
+                            Console.Write('X');
+                        }
+                    }
+                    j++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
